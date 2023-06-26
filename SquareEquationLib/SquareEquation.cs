6c205d26d10049802d9ac6e648d562f342813a7e
maxevenz <maxickm.pimun@gmail.com>
2023-06-26 13:29:17 +0000

    Updated SquareEquation.cs (3)

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -5,10 +5,10 @@ public class SquareEquation
     public static double[] Solve(double a, double b, double c)
     {
         double eps = 1e-9;
-        if (a==0 || a==double.NaN || a==double.NegativeInfinity || a==double.PositiveInfinity || 
-        b==double.NaN || b==double.NegativeInfinity || b==double.PositiveInfinity || 
-        c==double.NaN || c==double.NegativeInfinity || c==double.PositiveInfinity || 
-        Double.IsInfinity(a) || Double.IsInfinity(b) || Double.IsInfinity(c)) throw new System.ArgumentException();
+        if (Math.Abs(a)<eps || (Double.IsNaN(a)) || (Double.IsNaN(b)) || (Double.IsNaN(c)) || 
+        (Double.IsNegativeInfinity(a)) || (Double.IsNegativeInfinity(b)) || (Double.IsNegativeInfinity(c)) ||
+        (Double.IsPositiveInfinity(a)) || (Double.IsPositiveInfinity(b)) || (Double.IsPositiveInfinity(c)) || 
+        (Double.IsInfinity(a)) || (Double.IsInfinity(b)) || (Double.IsInfinity(c))) throw new System.ArgumentException();
         double[] o = new double[] {};
         double d;
         d=b*b-4*c*a;
@@ -18,7 +18,7 @@ public class SquareEquation
             o = new double[1];
             o[0] = -b/2;
         }
-        if (d>=eps)
+        else if (d>=eps)
         {
             o = new double[2];
             x1=-(b+Math.Sign(b)*Math.Sqrt(d))/2;
